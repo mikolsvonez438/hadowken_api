b4b143af8c7777516913e1cd69c0b16e86b5b777
mikolsvonez438 <mikolsvonez@gmail.com>
2026-03-04 18:37:13 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1081,5 +1081,4 @@ def generate_account_token(user, account_id):
         
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)})
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+
